ad9fa7e7b97cbc397db5154f066ee31ed56c7905
Oussama Fayz <fzoussfr@gmail.com>
2024-04-29 17:13:04 +0100

    removed tournament from the aside nav

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -31,7 +31,6 @@
             <ul class="list-unstyled d-flex flex-column  align-items-start  p-4  pt-5  ps-5 ">
               <li class="link mb-4 text-start  "><a href="dashboard.php" class="fs-5 text-dark fw-bold text-decoration-none "><i class="fa-solid fa-house"></i> Tableau de bord</a></li>
               <li class="link mb-4 text-start  "><a href="members/members.php" class="fs-5 fw-light text-dark text-decoration-none "><i class="fa-solid fa-users"></i> Membres</a></li>
-              <li class="link mb-4 "><a href="tournaments/tournaments.php" class="fs-5 fw-light text-dark text-decoration-none "><i class="fa-solid fa-trophy"></i> Tournois</a></li>
               <li class="link mb-4 "><a href="events/events.php" class="fs-5 text-dark fw-light text-decoration-none "><i class="fa-solid fa-calendar"></i> Evénements</a></li>
               <li class="link mb-4"><a href="transactions/transactions.php" class="fs-5 text-dark fw-light text-decoration-none "><i class="fa-solid fa-money-bill-transfer"></i> Transactions</a></li>
               <li class="link mb-4"><a href="trainingsessions/trainingsessions.php" class="fs-5 text-dark fw-light text-decoration-none "><i class="fa-solid fa-rectangle-list"></i> Séances d'entrainements</a></li>
